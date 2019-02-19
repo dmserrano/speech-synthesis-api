6f9cb9a8332b19062afc4a3dc544af8baf816c40
DominicSerrano <dmsrojo@gmail.com>
2019-02-19 09:14:58 -0600

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,44 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+
+    env: {
+        node: true
+    },
+
+    extends: [
+        "plugin:vue/essential",
+        "plugin:vue/recommended"
+    ],
+
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+
+        "quotes": [
+            "error",
+            "double"
+        ],
+
+        "semi": [
+            "error",
+            "always"
+        ],
+
+        // Specific vue/eslint rules
+        "vue/html-indent": ["error", 4],
+
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": 2,
+        }]
+    },
+
+    parserOptions: {
+        parser: "babel-eslint"
+    }
 };
